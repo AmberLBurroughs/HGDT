964029e09380118e76c319f89e8898b7cf803d5e
Amber Burroughs <amberlburroughs@gmail.com>
2018-03-30 16:20:24 -0700

    add webpack

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
-import logo from './logo.svg';
 import Accordions from './Components/Accordion';
 import Button from './Components/Buttons';
 import './App.css';
